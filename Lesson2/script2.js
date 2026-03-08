d7b1df35b1ceb7302380f05f082d3ca889334750
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-03-08 09:50:23 +0700

    Lesson 2

--- Lesson2/script2.js
+++ Lesson2/script2.js
@@ -19,3 +19,30 @@ const savedTheme = localStorage.getItem("theme");
 if (savedTheme === "dark") {
     document.body.classList.add("dark");
 }
+
+// Bài 2: Note List
+    // Lấy ghi chú trong localStorage hoặc khởi tạo danh sách rỗng
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
+    // Hàm hiển thị danh sách ghi chú lên trang
+function displayNotes() {
+     const list = document.getElementById("noteList");
+    list.innerHTML = "";
+    notes.forEach((item, index) => {
+        list.innerHTML += `<li>${item}</li>`;
+    });
+}
+displayNotes();;
+    // Sự kiện ấn nút (thêm ghi chú)
+const addNoteBtn = document.getElementById("addNote");
+addNoteBtn.onclick = () => {
+    // lấy dữ liệu trong ô input
+    let content = document.getElementById("noteInput").value.trim();
+    if (content) {
+        // Thêm ghi chú mới vào mảng
+        notes.push(content);
+        // Lưu mảng ghi chú vào localStorage
+        localStorage.setItem('notes', JSON.stringify(notes));
+        // Hiển thị lại danh sách ghi chú
+        displayNotes();
+    }
+};
